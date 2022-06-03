8725cfee86ecce80f7636b6287bf354ea70a4bb7
t0mdd <tomdd@protonmail.com>
2022-06-03 17:16:06 +0100

    Add calculator logic in script file and refactored
    The logic has variables storing the first/second operand and the
    operator, with values undefined if none has been input. Each time a
    button is clicked, a block of code (corresponding to which of these
    variables are currently defined or not and whether the first operand
    has been finalized or not) is ran. I refactored some repeated code in
    the script creation of certain html elements.

--- script.js
+++ script.js
@@ -15,36 +15,34 @@ const calculatorDisplay = document.querySelector('.calculatorDisplay');
 let currentOperation;
 let firstOperand;
 let secondOperand;
+let firstNumberEntered = false;
 
-function updateState(buttonClicked){
-  return;
+function clearResults(){
+  firstNumberEntered = false;
+  firstOperand = currentOperation = secondOperand = undefined;
+  calculatorDisplay.textContent = '';
 }
 
-function createNumberButton(number){
+document.querySelector('#clear').addEventListener('click',clearResults);
+
+function createButton(className,id,textContent){
   let button = document.createElement('button');
-  button.class = 'number';
-  button.textContent = number;
-  button.id = number;
+  button.className = className;
+  button.id = id;
+  button.textContent = textContent;
   button.addEventListener('click', e => updateState(e.target));
   return button;
+}
+function createNumberButton(number){
+  return createButton('number',number,number);
 }
 
 function createOperationButton(operation){
-  let button = document.createElement('button');
-  button.class = 'operation';
-  button.textContent = operation;
-  button.id = operation;
-  button.addEventListener('click', e => updateState(e.target));
-  return button;
+  return createButton('operation',operation,operation);
 }
 
 function createEqualsButton(){
-  let button = document.createElement('button');
-  button.class = 'evaluation';
-  button.textContent = '=';
-  button.id = '=';
-  button.addEventListener('click', e => updateState(e.target));
-  return button;
+  return createButton('evaluation','=','=')
 }
 
 function setupInputArea(){
@@ -63,5 +61,146 @@ function setupInputArea(){
                    createUpperRow(1,2,3,'-'),bottomRow);
 }
 
+function clearDisplay(){
+  calculatorDisplay.textContent = '';
+}
+
+function appendNumberToDisplay(numString){
+  if(calculatorDisplay.textContent === '0'){
+    if(numString !== '0'){
+      calculatorDisplay.textContent = numString;
+    }
+  }
+  else{
+    calculatorDisplay.textContent += numString;
+  }
+}
+
+function updateState(buttonClicked){
+  let buttonClass = buttonClicked.className;
+  let buttonId = buttonClicked.id;
+
+  if(firstOperand === undefined && currentOperation === undefined &&
+    secondOperand === undefined){
+    if(buttonClass === 'number'){
+      calculatorDisplay.textContent = buttonId;
+      firstOperand = +buttonId;
+    }
+    else if(buttonClass === 'operation'){
+      clearDisplay();
+      currentOperation = buttonId;
+    }
+    else if(buttonClass === 'evaluation'){
+      clearDisplay();
+    }
+  }
+
+  else if(firstOperand !== undefined &&
+    currentOperation === undefined && secondOperand === undefined){
+    if(buttonClass === 'number'){
+      if(firstNumberEntered){
+        calculatorDisplay.textContent = buttonId;
+        firstOperand = +buttonId;
+        firstNumberEntered = false;
+      }
+      else{
+        appendNumberToDisplay(buttonId);
+        firstOperand = +calculatorDisplay.textContent;
+      }
+    }
+    else if(buttonClass === 'operation'){
+      currentOperation = buttonId;
+      firstNumberEntered = true;
+    }
+    else if(buttonClass === 'evaluation'){
+      firstNumberEntered = true;
+    }
+  }
+
+  else if(firstOperand !== undefined &&
+    currentOperation !== undefined && secondOperand === undefined){
+    if(buttonClass === 'number'){
+      clearDisplay();
+      appendNumberToDisplay(buttonId);
+      secondOperand = +buttonId;
+    }
+    else if(buttonClass === 'operation'){
+      currentOperation = buttonId;
+    }
+    else if(buttonClass === 'evaluation'){
+      // acts as a way to cancel the operation
+      currentOperation = undefined;
+    }
+  }
+
+  else if(firstOperand !== undefined &&
+    currentOperation !== undefined && secondOperand !== undefined){
+    if(buttonClass === 'number'){
+      appendNumberToDisplay(buttonId);
+      secondOperand = +calculatorDisplay.textContent;
+    }
+    else if(buttonClass === 'operation' ||
+      buttonClass === 'evaluation'){
+      if(currentOperation === '/' && secondOperand === 0){
+        clearResults();
+        calculatorDisplay.textContent =
+          'No division by 0 allowed, you maniac.';
+      }
+      else{
+        firstOperand =
+          operate(currentOperation,firstOperand,secondOperand);
+        calculatorDisplay.textContent = firstOperand;
+        secondOperand = undefined;
+        currentOperation = (buttonClass === 'operation') ?
+          buttonId : undefined;
+      }
+    }
+  }
+
+  else if(firstOperand === undefined &&
+    currentOperation !== undefined && secondOperand === undefined){
+    if(buttonClass === 'number'){
+      calculatorDisplay.textContent = buttonId;
+      secondOperand = +buttonId;
+    }
+    else if(buttonClass === 'operation'){
+      currentOperation = buttonId;
+    }
+    else if(buttonClass === 'evaluation'){
+      currentOperation.textContent = 'The operation needs a number';
+      clearResults();
+    }
+  }
+
+  // unary operations
+  else if(firstOperand === undefined &&
+    currentOperation !== undefined && secondOperand !== undefined){
+    if(buttonClass === 'number'){
+      appendNumberToDisplay(buttonId);
+      secondOperand = +calculatorDisplay.textContent;
+    }
+    else if(buttonClass === 'operation' ||
+      buttonClass === 'evaluation'){
+      if(['/','*'].includes(currentOperation)){
+        calculatorDisplay.textContent = 'Division and ' +
+          'multiplication need to take a number before being clicked';
+        clearResults();
+      }
+      else{
+        firstOperand =
+          operate(currentOperation,0,secondOperand);
+        calculatorDisplay.textContent = firstOperand;
+        secondOperand = undefined;
+        if(buttonClass === 'operation'){
+          currentOperation = buttonId;
+        }
+        else if(buttonClass === 'evaluation'){
+          currentOperation = undefined;
+        }
+      }
+    }
+  }
+
+}
 
 setupInputArea();
